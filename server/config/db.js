ec3894f34bfbc98b1676199d23ad6fe421a5c109
akashindex <it.asdf303@gmail.com>
2025-09-22 07:12:04 -0700

    changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose');
-const { MongoUrl } = require('./config');
+require('dotenv').config(); // load .env file
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MongoUrl, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     });
 
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // Exit process with failure
     process.exit(1);
   }
 };
- 
+
 module.exports = connectDB;
